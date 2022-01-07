9b33e93f461ce4dc2463d3be778850f764770a9c
Roopa2615 <rupashree1011@gmail.com>
2022-01-07 15:33:28 +0530

    draggable UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import React, { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 function App() {
+  const data = [
+    { name: "Apple", price: 1 },
+    { name: "Banana", price: 3 },
+    { name: "Cherry", price: 5 },
+    { name: "Grapes", price: 7 },
+    { name: "Mango", price: 2 },
+    { name: "Orange", price: 3 },
+    { name: "Strawberry", price: 5 },
+  ];
+
+  const [items, setItems] = useState(data);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    console.log(result.source.index, result.destination.index);
+    const newItem = Array.from(items);
+    const [reorderedItem] = newItem.splice(result.source.index, 1);
+    newItem.splice(result.destination.index, 0, reorderedItem);
+    setItems(newItem);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Items</h1>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="items">
+          {(provided) => (
+            <ul
+              className="items"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {items
+                // .sort((a, b) =>
+                //   a.price === b.price
+                //     ? a.name > b.name
+                //       ? 1
+                //       : -1
+                //     : a.price - b.price
+                // )
+                .map((item, index) => {
+                  return (
+                    <Draggable
+                      key={item.name}
+                      draggableId={item.name}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          id="items"
+                        >
+                          {item.name} = {item.price}
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
